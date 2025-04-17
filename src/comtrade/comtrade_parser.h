e2992b731b8dbc4281e8a849d94b7efa6fc05d74
kosteranu <101293397+kosteranu@users.noreply.github.com>
2025-04-17 19:25:29 +0300

    added class ComtradeFile
    fixed previous classes and added class ComtradeFile, that is needed to be enhanced

--- src/comtrade/comtrade_parser.h
+++ src/comtrade/comtrade_parser.h
@@ -11,85 +11,81 @@
 namespace comtrade {
 
 /**
-* Исключение выводимое при несоотвествии введенного значения ограничения стандарта IEC COMTRADE
-*/
-const std::string kExceptionForInitParam = "Inappropriate value!"; 
-
-/**
   * Данная функция проверяет вводимое значение типа std::string для инициализации атрибута класса 
   * AnalogSignal или DigitalSignal на соотвествие ограничениям стандарта IEC COMTRADE
   * @param con условие проверки вводимого значения для иницилизируемого атрибута класса 
   * AnalogSignal или DigitalSignal согласно стандарту IEC COMTRADE
-  * @param var вводимое значения для иницилизируемого атрибута класса AnalogSignal или DigitalSignal
-  * @param param указатель на иницилизируемый атрибут класса AnalogSignal или DigitalSignal
+  * @param value вводимое значения для иницилизируемого атрибута класса AnalogSignal или DigitalSignal
+  * @param param ссылка на иницилизируемый атрибут класса AnalogSignal или DigitalSignal
+  * @param min_value минимальное значение, соответствующее ограничениям стандарта
+  * @param max_value максимальное значение, соответствующее ограничениям стандарта
   */
-inline void ConForString(bool con, const std::string& var, std::string& param) {
-  if(con) param = var;
-  else throw std::invalid_argument("Invalid value!");
+inline void CheckParam(std::string& value, std::string& param, 
+                       size_t min_value = std::numeric_limits<uint8_t>::min(),
+                       size_t max_value = std::numeric_limits<uint8_t>::max()) {
+  if (value.length() >= min_value && value.length() <= max_value) {
+    param = value;
+  } else {
+    throw std::invalid_argument("Inappropriate value!");
+  }
 }
 /**
   * Данная функция проверяет вводимое значение типа int32_t для инициализации атрибута класса 
   * AnalogSignal или DigitalSignal на соотвествие ограничениям стандарта IEC COMTRADE
   * @param con условие проверки вводимого значения для иницилизируемого атрибута класса 
   * AnalogSignal или DigitalSignal согласно стандарту IEC COMTRADE
-  * @param var вводимое значения для иницилизируемого атрибута класса AnalogSignal или DigitalSignal
-  * @param param указатель на иницилизируемый атрибут класса AnalogSignal или DigitalSignal
+  * @param value вводимое значения для иницилизируемого атрибута класса AnalogSignal или DigitalSignal
+  * @param param ссылка на иницилизируемый атрибут класса AnalogSignal или DigitalSignal
+  * @param min_value минимальное значение, соответствующее ограничениям стандарта
+  * @param max_value максимальное значение, соответствующее ограничениям стандарта
   */
-inline void ConForInt32(bool con, int32_t var, int32_t* param) {
-  if(con) *param = var;
-  else throw kExceptionForInitParam;
-}
-
-void CheckParam(float value, float& out,
-  float min_value = std::numeric_limits<float>::min(),
-  float max_value = std::numeric_limits<float>::max()) {
+inline void CheckParam(int32_t& value, int32_t& param,
+                       int32_t min_value = std::numeric_limits<int32_t>::min(),
+                       int32_t max_value = std::numeric_limits<int32_t>::max()) {  
   if (value >= min_value && value <= max_value) {
-    out = value;
+    param = value;
   } else {
-    throw std::out_of_range("Float value out of range!");
+    throw std::invalid_argument("Inappropriate value!");
   }
 }
-
-void CheckParam(int32_t value, int32_t& out,
-  int32_t min_value = std::numeric_limits<int32_t>::min(),
-  int32_t max_value = std::numeric_limits<int32_t>::min()) {
-  if (value > min_value && value < max_value) {
-    out = value;
-  } else {
-    throw std::out_of_range("Float value out of range!");
-  }
-}
-
-
 /**
   * Данная функция проверяет вводимое значение типа float для инициализации атрибута класса 
   * AnalogSignal или DigitalSignal на соотвествие ограничениям стандарта IEC COMTRADE
   * @param con условие проверки вводимого значения для иницилизируемого атрибута класса 
   * AnalogSignal или DigitalSignal согласно стандарту IEC COMTRADE
-  * @param var вводимое значения для иницилизируемого атрибута класса AnalogSignal или DigitalSignal
-  * @param param указатель на иницилизируемый атрибут класса AnalogSignal или DigitalSignal
+  * @param value вводимое значения для иницилизируемого атрибута класса AnalogSignal или DigitalSignal
+  * @param param ссылка на иницилизируемый атрибут класса AnalogSignal или DigitalSignal
+  * @param min_value минимальное значение, соответствующее ограничениям стандарта
+  * @param max_value максимальное значение, соответствующее ограничениям стандарта
   */
-inline void ConForFloat(bool con, float var, float* param) {
-  if(con) *param = var;
-  else throw kExceptionForInitParam;
+inline void CheckParam(float& value, float& param,
+                       float min_value = std::numeric_limits<float>::min(),
+                       float max_value = std::numeric_limits<float>::max()) {
+  if (value >= min_value && value <= max_value) {
+    param = value;
+  } else {
+    throw std::invalid_argument("Inappropriate value!");
+  }
 }
 /**
   * Данная функция проверяет вводимое значение типа char для инициализации атрибута класса 
   * AnalogSignal или DigitalSignal на соотвествие ограничениям стандарта IEC COMTRADE
   * @param con условие проверки вводимого значения для иницилизируемого атрибута класса 
   * AnalogSignal или DigitalSignal согласно стандарту IEC COMTRADE
-  * @param var вводимое значения для иницилизируемого атрибута класса AnalogSignal или DigitalSignal
-  * @param param указатель на иницилизируемый атрибут класса AnalogSignal или DigitalSignal
+  * @param value вводимое значения для иницилизируемого атрибута класса AnalogSignal или DigitalSignal
+  * @param param ссылка на иницилизируемый атрибут класса AnalogSignal или DigitalSignal
+  * @param condition условие из стандарта для проверки введенного значения
   */
-inline void ConForChar(bool con, char var, char* param) {
-  if(con) *param = var;
-  else throw kExceptionForInitParam;
+inline void CheckParam(char& value, char& param, bool condition) {
+  if(condition) param = value;
+  else throw std::invalid_argument("Inappropriate value!");
 }
 
 class ComtradeParser {
  public:
   ComtradeParser();
 };
+
 
 /**
  * Класс является описанием аналогового сигнала согласно стандарту IEC COMTRADE.
@@ -125,20 +121,20 @@ class AnalogSignal {
   AnalogSignal(int32_t An, std::string ch_id, std::string uu, float a, float b, 
                float skew, float min, float max, float primary, float secondary, 
                char PS, std::string ph="", std::string ccbm="") {
-    CheckParam(An,An_, 0, 9999);
-    ConForString((ch_id.length() > 0 && ch_id.length() <= 128), ch_id, ch_id_);
-    ConForString((ph.length() <= 2), ph, ph_);
-    ConForString((ccbm.length() <= 64), ccbm, ccbm_);
-    ConForString((uu.length() > 0 && uu.length() <= 32), uu, uu_);
+    CheckParam(An, An_, 1, 999999); 
+    CheckParam(ch_id, ch_id_, 1, 128);
+    CheckParam(ph, ph_, 0, 2);
+    CheckParam(ccbm, ccbm_, 0, 64);
+    CheckParam(uu, uu_, 1, 32);
     a_ = a;
     b_ = b;
     skew_ = skew;
-    if(min > max) throw kExceptionForInitParam;
-    ConForFloat((min > -3.4028236e38 && min < 3.4028236e38), min, &min_);
-    ConForFloat((max > -3.4028236e38F && max < 3.4028236e38F), max, &max_);
+    if(min > max) throw std::invalid_argument("Inappropriate value!");
+    CheckParam(min, min_);
+    CheckParam(max, max_);
     primary_ = primary;
     secondary_ = secondary;
-    ConForChar((PS == 'P' || PS == 'p' || PS == 'S' || PS == 's'), PS, &PS_);
+    CheckParam(PS, PS_, (PS == 'P' || PS == 'p' || PS == 'S' || PS == 's'));
   }
 
   int32_t GetAn() {
@@ -194,10 +190,10 @@ class DigitalSignal {
   bool y_; // Нормальное состояние канала состояния. Критичные данные, целое число (0/1)
  public:
   DigitalSignal(int32_t Dn, std::string ch_id, bool y, std::string ph="", std::string ccbm="") {
-    ConForInt32((Dn > 0 && Dn <= 999999), Dn, &Dn_);
-    ConForString((ch_id.length() > 0 && ch_id.length() <= 128), ch_id, ch_id_);
-    ConForString((ph.length() <= 2), ph, ph_);
-    ConForString((ccbm.length() <= 64), ccbm, ccbm_);
+    CheckParam(Dn, Dn_, 1, 999999);
+    CheckParam(ch_id, ch_id_, 1, 128);
+    CheckParam(ph, ph_, 0, 2);
+    CheckParam(ccbm, ccbm_, 0, 64);
     y_ = y;
   }
 
@@ -222,21 +218,83 @@ class DigitalSignal {
  * Класс создает вектор аналоговых сигналов и вектор цифровых сигналов
  */
 class ComtradeFile {
+  std::string station_name_;
+  std::string rec_dev_id_;
+  short rev_year_ = (short)1991;
+  int32_t TT_;
+  int32_t analog_count_;
+  int32_t digital_count_;
+  float lf_;
+  uint8_t nrates_;
+  float samp_;
+  int64_t endsamp_;
+  std::string time_start_;
+  std::string trigger_point_;
+  std::string ft_;
+  float timemult_;
+  std::string time_code_;
+  std::string local_code_;
+  int8_t tmq_code_;
+  int8_t leapsec_;
 
   std::vector<AnalogSignal> analog_vector; // Вектор состоящий из аналоговых сигналов
   std::vector<DigitalSignal> digital_vector; // Вектор состоящий из цифровых сигналов
-public:
-  void PushAnalog(const AnalogSignal& signal) {
-    analog_vector.push_back(signal);
+ public: 
+  ComtradeFile(std::string station_name,
+               std::string rec_dev_id,
+               short rev_year,
+               int32_t TT,
+               int32_t analog_count,
+               int32_t digital_count,
+               float lf,
+               uint8_t nrates,
+               float samp,
+               int64_t endsamp,
+               std::string time_start,
+               std::string trigger_point,
+               std::string ft,
+               float timemult,
+               std::string time_code,
+               std::string local_code,
+               int8_t tmq_code,
+               int8_t leapsec) {
+    CheckParam(station_name, station_name_, 0, 64);
+    CheckParam(rec_dev_id, rec_dev_id_, 0, 64);
+    //CheckParam(rev_year, rev_year, 0, 64);
+    if(TT != analog_count + digital_count) throw std::invalid_argument("Inappropriate count of analog or digital signals!");
+    CheckParam(TT, TT_, 1, 999999);
+    CheckParam(analog_count, analog_count_, 0, 999999);
+    CheckParam(digital_count, digital_count_, 0, 999999);
+    CheckParam(lf, lf_);
+    //nrates
+    CheckParam(samp, samp_);
+    //endsamp
+    //function for time
+    //function for time
+    CheckParam(ft, ft_, 5, 8); // доппроверки
+    CheckParam(timemult, timemult_);
+    CheckParam(time_code, time_code_);
+    CheckParam(local_code, local_code_);
+    //CheckParam(tmq_code, tmq_code_, (tmq_code >= 0 && tmq_code < 16));
+    //CheckParam();
+
+    analog_vector.reserve(analog_count);
+    digital_vector.reserve(digital_count);
   }
-  void PushDigital(DigitalSignal signal) {
-    digital_vector.push_back(signal);
+  
+  void PushAnalog(AnalogSignal& signal) {
+    static int32_t number = 0;
+    analog_vector.at(number++) = signal;
   }
-  const AnalogSignal& GetAnalogSignal(std::size_t an) {
+  void PushDigital(DigitalSignal& signal) {
+    static int32_t number = 0;
+    digital_vector.at(number++) = signal;
+  }
+  const AnalogSignal& GetAnalogSignal(size_t an) {
     return analog_vector.at(an);
   }
-  DigitalSignal GetDigitalSignal(int32_t Dn) {
-    return digital_vector[Dn];
+  const DigitalSignal& GetDigitalSignal(size_t dn) {
+    return digital_vector.at(dn);
   }
 };
 
